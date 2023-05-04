138f34ee86632afd0794046bca8cd3757c1aea3d
paola <pebitaa5795@hotmail.com>
2023-05-04 14:35:47 -0600

    polimorfismo

--- Ejemplo3.php
+++ Ejemplo3.php
@@ -19,4 +19,29 @@ class Transporte {
     }
 }   
 
+ class Bicicleta extends Transporte {
+
+    public function getInfo() : string {
+        return "el transporte tiene " . $this->ruedas . " ruedas y una capacidad de " . $this->capacidad . " personas y NO GASTA GASOLINA";
+        }
+ }
+
+ class Automovil extends Transporte {
+
+    protected string $transmision;
+    public function __construct(int $ruedas, int $capacidad, string $transmision)
+    {
+        $this->ruedas = $ruedas;
+        $this->capacidad = $capacidad;
+        $this->transmision = $transmision;
+
+    }
+
+    public function getTransmision() : string {
+        return $this->transmision;
+
+    } 
+ }
+
+ 
 
